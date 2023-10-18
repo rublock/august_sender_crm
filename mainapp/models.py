f272b93919543ff20e8c641f2a59926392fb7718
mack@ubuntu <hypermail@yandex.ru>
2023-10-18 06:50:59 -0700

    commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -17,3 +17,28 @@ class Client(models.Model):
     class Meta:
         verbose_name = "Клиент"
         verbose_name_plural = "Клиенты"
+
+
+class Product(models.Model):
+    name = models.CharField(verbose_name="Наименование", max_length=100)
+    material = models.CharField(verbose_name="Материал", blank=True, max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Изделие"
+        verbose_name_plural = "Изделия"
+
+
+class Order(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=100)
+    order_product = models.ManyToManyField(Product)
+    ordr_client = models.ManyToManyField(Client)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"

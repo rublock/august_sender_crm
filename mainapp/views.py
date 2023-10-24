06c3a623d74f84529f9add5ec6f7be85600a29ff
mack <hypermail@yandex.ru>
2023-10-24 13:57:54 +0300

    commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -7,5 +7,14 @@ from .models import OrderPosition
 
 @require_http_methods(['GET'])
 def order_positions_list(request):
-    order_positions_list = OrderPosition.objects.all()
-    return render(request, 'home_page.html', {'order_positions_list': order_positions_list})
+    order_positions_list_status_1 = OrderPosition.objects.filter(status=1)
+    order_positions_list_status_2 = OrderPosition.objects.filter(status=2)
+    order_positions_list_status_3 = OrderPosition.objects.filter(status=3)
+    order_positions_list_status_4 = OrderPosition.objects.filter(status=4)
+
+    return render(request, 'home_page.html', {
+        'order_positions_list_status_1': order_positions_list_status_1,
+        'order_positions_list_status_2': order_positions_list_status_2,
+        'order_positions_list_status_3': order_positions_list_status_3,
+        'order_positions_list_status_4': order_positions_list_status_4,
+        })

e5a9affbaaa49194ab2a922a70154a7b25fda086
mack@ubuntu <hypermail@yandex.ru>
2023-10-31 00:45:30 -0700

    commit ok

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -19,7 +19,8 @@ class NewOrderForm(forms.Form):
     }))
     description = forms.CharField(
         label="Примечание", max_length=200, required=False, widget=forms.Textarea(attrs={
-            'class': 'form-control', 'rows': 3
+            'class': 'form-control',
+            'rows': 3
         })
     )
     status = forms.ChoiceField(label="Статус", choices=CHOICES, initial=1, widget=forms.Select(attrs={
@@ -30,13 +31,18 @@ class NewOrderForm(forms.Form):
 class NewClientForm(forms.Form):
     name = forms.CharField(label="ФИО", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     contact = forms.CharField(label="Контакт", max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    where_from = forms.CharField(label="Источник заказа", max_length=200, widget=forms.TextInput(attrs={
+    where_from = forms.CharField(label="Источник заказа", required=False, max_length=200, widget=forms.TextInput(attrs={
         'class': 'form-control'
     }))
-    oder_details = forms.CharField(label="Индивидуальные условия заказа", max_length=200, widget=forms.TextInput(attrs={
-        'class': 'form-control'
+    oder_details = forms.CharField(label="Индивидуальные условия заказа", required=False, max_length=200,
+                                   widget=forms.TextInput(attrs={
+                                       'class': 'form-control'
+                                   }))
+    address = forms.CharField(label="Адрес доставки", required=False, max_length=200, widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': 3,
     }))
-    address = forms.CharField(label="Адрес доставки", max_length=200, widget=forms.TextInput(attrs={
-        'class': 'form-control'
+    notes = forms.CharField(label="Заметки", required=False, max_length=200, widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': 3,
     }))
-    notes = forms.CharField(label="Заметки", max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
